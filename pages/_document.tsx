b28f3e5834dd405c210078f1493638340f6d979b
Mao De Matos <mao.dematos@gmail.com>
2022-09-24 19:38:37 +0200

    🔧 Add `twin.macro`, `daisyui` & `stitches`

---
+++ pages/_document.tsx
@@ -0,0 +1,38 @@
+import NextDocument, { Head, Html, Main, NextScript } from "next/document";
+
+import { getCssText } from "../stitches.config";
+
+export default class Document extends NextDocument {
+  static async getInitialProps(ctx: any) {
+    try {
+      const initialProps = await NextDocument.getInitialProps(ctx);
+
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {/* Stitches CSS for SSR */}
+            <style
+              id="stitches"
+              dangerouslySetInnerHTML={{ __html: getCssText() }}
+            />
+          </>
+        ),
+      };
+    } finally {
+    }
+  }
+
+  render() {
+    return (
+      <Html lang="en" data-theme>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
