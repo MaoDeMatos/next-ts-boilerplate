1818d5b45040802552a33abc9c3bd6842eef48dc
Mao De Matos <mao.dematos@gmail.com>
2022-10-04 21:11:21 +0200

    ⚡️ Change loading of initial script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,6 @@
 import { Head, Html, Main, NextScript } from "next/document";
-import Script from "next/script";
 
+// import Script from "next/script";
 import { getCssText } from "../stitches.config";
 
 export default function Document() {
@@ -11,15 +11,23 @@ export default function Document() {
           id="stitches"
           dangerouslySetInnerHTML={{ __html: getCssText() }}
         />
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html:
+              '(()=>{const e=localStorage.getItem("theme-storage"),t=window.matchMedia&&window.matchMedia("(prefers-color-scheme: dark)").matches,a=t?"dracula":"light",r=document.documentElement;if(!e)return r.setAttribute("data-theme",a),void localStorage.setItem("theme-storage",JSON.stringify({state:{currentTheme:"system",isDarkMode:t}}));const{state:s}=JSON.parse(e);"system"===s.currentTheme?r.setAttribute("data-theme",a):r.setAttribute("data-theme",s.currentTheme)})();',
+          }}
+        />
       </Head>
+
       <body>
         <Main />
         <NextScript />
-        <Script
+        {/* <Script
           id="theme-initializer"
           strategy="beforeInteractive"
           src="./headerScript.min.js"
-        />
+        /> */}
       </body>
     </Html>
   );
